50f9a1321f931b53f257077d36de5f45db174480
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-12 17:04:01 -0400

    Added choice input option

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -1,11 +1,11 @@
 print("Welcome to the Bank of Blooket!")
 
-print("Thank you for using the Bank of Blooket!",
-        "1. Open an account",
-        "2. Withdraw Money from bank",
-        "3. Deposit Money",
-        "4. View balance",
-        "5. Close Account"
+print("Thank you for using the Bank of Blooket!, Your options are:\n",
+        "1. Open an account\n",
+        "2. Withdraw Money from bank\n",
+        "3. Deposit Money\n",
+        "4. View balance\n",
+        "5. Close Account\n"
      )
 
 choice = input("Which one would you like to do?")
