d7ff33335253b3cafb40b167e866a0f58e854f18
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-12 16:50:58 -0400

    Added menu chocice

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -1,9 +1,11 @@
 print("Welcome to the Bank of Blooket!")
 
-print("Thank you for using the Bank of Blooket. What would you like to do?",
+print("Thank you for using the Bank of Blooket!",
         "1. Open an account",
         "2. Withdraw Money from bank",
         "3. Deposit Money",
         "4. View balance",
         "5. Close Account"
      )
+
+choice = input("Which one would you like to do?")
