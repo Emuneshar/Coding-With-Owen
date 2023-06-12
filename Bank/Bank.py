428b50bfe467378cf616b985a219144c952942fc
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-12 16:48:48 -0400

    modified menu

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -5,6 +5,5 @@ print("Thank you for using the Bank of Blooket. What would you like to do?",
         "2. Withdraw Money from bank",
         "3. Deposit Money",
         "4. View balance",
-        "5. Close Account",
-        "6. Take someone off account"     
-      )
+        "5. Close Account"
+     )
