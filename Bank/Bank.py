a5213799e95a1f2c1a2b8f31ea4332096ae3a4ac
Eshwar Muneshar <Emuneshar@gmail.com>
2023-06-19 17:25:12 -0400

    created a user

--- Bank/Bank.py
+++ Bank/Bank.py
@@ -1,26 +1,6 @@
+import User.py
+
 print("Welcome to the Bank of Blooket!")
-
-
-# Python Data Types
-
-# Strings - words/sentences
-# Booleans - True or False
-# Floats - Numbers with decimals
-# Int - Numbers without decimals
-# User - Name, Bankaccount Number, Balance, Deposit, Withdraw, 
-
-
-
-
-
-# Level 0 - Scratch
-# Level 1 - Python, Java, (HTML, CSS, Javascript), SQL
-# Level 2 - C, C++, C#, LISP
-# Level 3 - MIPS, Machine Language, Assembly
-
-
-
-
 
 
 print("Thank you for using the Bank of Blooket!, Your options are:\n",
@@ -30,6 +10,8 @@ print("Thank you for using the Bank of Blooket!, Your options are:\n",
         "4. View balance\n",
         "5. Close Account\n"
      )
+
+Bob = User("Bob", "Nugget", "BNugget", "chicken52", 1234658, 5)
 
 choice = int(input("Which one would you like to do?"))
 
